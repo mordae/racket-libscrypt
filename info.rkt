544c692f6d492275002d55fc933049e4abff56d8
Jan Dvořák <mordae@anilinux.org>
2016-01-14 09:49:58 +0100

    Add missing dependency on unstable-lib
    Signed-off-by: Jan Dvořák <mordae@anilinux.org>

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang setup/infotab
 
 (define collection 'multi)
-(define deps '("base" "scribble-lib"))
-(define build-deps '("racket-doc"))
+(define deps '("base" "scribble-lib" "unstable-lib"))
+(define build-deps '("racket-doc" "unstable-lib"))
 
 ; vim:set ts=2 sw=2 et:
